d190196fe046112121721627b005a0f46eb5e7aa
fresh5447 <doug@kosmojo.com>
2017-07-28 15:47:08 -0600

    full stack setup

--- client/src/App.js
+++ client/src/App.js
@@ -19,11 +19,24 @@ class App extends Component {
 
   componentDidMount() {
     this.makeProducts()
-    setInterval(() => {
-      this.setState((prevState) => ({
-        opacity: prevState.opacity === 1 ? 0.5 : 1
-      }))
-    }, 2500)
+
+    let authObj = {
+                    method: 'GET',
+                    headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/x-www-form-urlencoded',
+                      'X-AUTH-TOKEN': '2d4c2733-aa49-48fa-9109-89519e25e4eb'
+                    }
+                  }
+    fetch('https://griz-atom-api.bequickapps.com/products', authObj)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+      })
+      .catch(e => {
+        console.log(e);
+        return e;
+      })
   }
 
   makeProducts(){
